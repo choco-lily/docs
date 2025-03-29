f8e788ff59cc752cb5e7c9cbd503f6e4c204b558
chocolily <medeia_beliar@naver.com>
2025-03-29 10:34:25 +0900

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {
+    defineConfig
+} from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
@@ -11,30 +13,31 @@ export default defineConfig({
                 twitter: 'https://x.com/Choco_Lily__',
                 github: 'https://github.com/choco-lily/chocolily.dev',
             },
-            sidebar: [
-                {
+            sidebar: [{
                     label: 'Guides',
                     items: [
                         // Each item here is one entry in the navigation menu.
-                        { label: 'Example Guide', slug: 'guides/example' },
+                        {
+                            label: 'Example Guide',
+                            slug: 'guides/example'
+                        },
                     ],
                 },
                 {
                     label: 'Reference',
-                    autogenerate: { directory: 'reference' },
+                    autogenerate: {
+                        directory: 'reference'
+                    },
                 },
             ],
-            head: [
-    // Example: add Fathom analytics script tag.
-    {
-      tag: 'script',
-      attrs: {
-        src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2420658305303725',
-        'crossorigin': 'anonymous',
-        defer: true,
-      },
-    },
-  ],
+            head: [{
+                tag: 'script',
+                attrs: {
+                    src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2420658305303725',
+                    crossorigin: 'anonymous',
+                    async: true,
+                },
+            }, ],
         }),
     ],
 });
