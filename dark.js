9d9542242101ed689578453baa4bbf291f7bd11b
chocolily <medeia_beliar@naver.com>
2025-03-31 18:50:07 +0900

    update dark.js

--- dark.js
+++ dark.js
@@ -19,51 +19,7 @@ fs.writeFileSync(
 
 fs.writeFileSync(
     "./dist/server/entry.mjs",
-    ```import { renderers } from './renderers.mjs';
-import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_1yFOOFk2.mjs';
-import { manifest } from './manifest_1rfzln_p.mjs';
-
-const serverIslandMap = new Map();;
-
-const _page0 = () => import('./pages/_image.astro.mjs');
-const _page1 = () => import('./pages/404.astro.mjs');
-const _page2 = () => import('./pages/api/auth/_---auth_.astro.mjs');
-const _page3 = () => import('./pages/auth/_---path_.astro.mjs');
-const _page4 = () => import('./pages/_---slug_.astro.mjs');
-const pageMap = new Map([
-    ["node_modules/astro/dist/assets/endpoint/node.js", _page0],
-    ["node_modules/@astrojs/starlight/routes/static/404.astro", _page1],
-    ["node_modules/auth-astro/src/api/[...auth].ts", _page2],
-    ["src/pages/auth/[...path].ts", _page3],
-    ["node_modules/@astrojs/starlight/routes/static/index.astro", _page4]
-]);
-
-const _manifest = Object.assign(manifest, {
-    pageMap,
-    serverIslandMap,
-    renderers,
-    actions: () => import('./_noop-actions.mjs'),
-    middleware: () => import('./_astro-internal_middleware.mjs')
-});
-const _args = {
-    "mode": "middleware",
-    "client": "",
-    "server": "",
-    "host": false,
-    "port": 4321,
-    "assets": "_astro"
-};
-const _exports = createExports(_manifest, _args);
-const handler = _exports['handler'];
-const startServer = _exports['startServer'];
-const options = _exports['options'];
-const _start = 'start';
-if (_start in serverEntrypointModule) {
-	serverEntrypointModule[_start](_manifest, _args);
-}
-
-export { handler, options, pageMap, startServer };
-```,
+    'import{renderers as s}from"./renderers.mjs";import{c as a,s as t}from"./chunks/_@astrojs-ssr-adapter_1yFOOFk2.mjs";import{manifest as e}from"./manifest_1rfzln_p.mjs";let serverIslandMap=new Map,_page0=()=>import("./pages/_image.astro.mjs"),_page1=()=>import("./pages/404.astro.mjs"),_page2=()=>import("./pages/api/auth/_---auth_.astro.mjs"),_page3=()=>import("./pages/auth/_---path_.astro.mjs"),_page4=()=>import("./pages/_---slug_.astro.mjs"),pageMap=new Map([["node_modules/astro/dist/assets/endpoint/node.js",_page0],["node_modules/@astrojs/starlight/routes/static/404.astro",_page1],["node_modules/auth-astro/src/api/[...auth].ts",_page2],["src/pages/auth/[...path].ts",_page3],["node_modules/@astrojs/starlight/routes/static/index.astro",_page4]]),_manifest=Object.assign(e,{pageMap,serverIslandMap,renderers:s,actions:()=>import("./_noop-actions.mjs"),middleware:()=>import("./_astro-internal_middleware.mjs")}),_args={mode:"middleware",client:"",server:"",host:!1,port:4321,assets:"_astro"},_exports=a(_manifest,_args),handler=_exports.handler,startServer=_exports.startServer,options=_exports.options,_start="start";_start in t&&t[_start](_manifest,_args);export{handler,options,pageMap,startServer};',
 )
 
 console.log("now default mode is dark!")
